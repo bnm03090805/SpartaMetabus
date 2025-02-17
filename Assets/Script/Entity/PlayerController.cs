4002b03748adc7543c76a3e74c708253e5e7e8e0
bnm21105 <bnm2513@hanmail.net>
2025-02-17 15:25:56 +0900

    [feat] 플레이어 이동기능 구현 및 패키지 임포트

---
+++ Assets/Script/Entity/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : BaseController
+{
+    private Camera camera;
+
+    protected override void Start()
+    {
+        base.Start();
+        camera = Camera.main;
+    }
+
+    protected override void HandleAction()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertial = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertial).normalized;
+
+        //Vector2 mousePosition = Input.mousePosition;
+        //Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+        lookDirection = this._rigidbody.velocity;
+
+        if (lookDirection.magnitude < .9f)
+        {
+            lookDirection = Vector2.zero;
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
+    }
+}
