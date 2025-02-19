c546253bd8bf41bd8abe2ffa036b44e2d54544ea
bnm21105 <bnm2513@hanmail.net>
2025-02-19 20:53:56 +0900

    [feat] NPC위에 UI 생성

--- Assets/Script/Entity/NPCInteraction.cs
+++ Assets/Script/Entity/NPCInteraction.cs
@@ -7,12 +7,19 @@ public class NPCInteraction : MonoBehaviour
     [SerializeField] GameObject interactKey;
     private void Start()
     {
-        interactKey = GameObject.Find("Canvas").transform.GetChild(2).gameObject;
-        Debug.Log(interactKey.name);
+        interactKey = GameObject.Find("NpcCanvas").transform.GetChild(0).gameObject;
+        //interactKey.transform.position = Camera.main.WorldToScreenPoint(this.transform.position);
+        //Debug.Log(interactKey.name);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log(collision.name);
+        interactKey.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        interactKey.SetActive(false);
     }
 }
