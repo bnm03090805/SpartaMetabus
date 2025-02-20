61b73255bb5eeb8f170f1c299666069cbd91da60
bnm21105 <bnm2513@hanmail.net>
2025-02-20 15:26:03 +0900

    [feat] 리더보드 UI 작업

--- Assets/Script/Entity/LeaderBoardPopupUI.cs
+++ Assets/Script/Entity/LeaderBoardPopupUI.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class LeaderBoardPopupUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject leaderBoardUI;
 
-    // Update is called once per frame
-    void Update()
+    public void OpenLeaderBoardUI()
     {
-        
+        UIManager.Instance.OpenUI(leaderBoardUI);
     }
 }
